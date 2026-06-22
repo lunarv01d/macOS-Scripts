d9bb765286208d871f92fe2054be82a5ec892946
Luna <rylanhughey@gmail.com>
2026-06-22 10:18:16 -0500

    Create UnBind

---
+++ UnBind_macOS.sh
@@ -0,0 +1,57 @@
+Celina Makinsi 6/22/2026 9:56 AM • #!/bin/bash
+
+loggedInUser=$(stat -f%Su /dev/console)
+
+if [[ "$loggedInUser" == "root" || -z "$loggedInUser" ]]; then
+
+echo "No user logged in."
+
+exit 1
+
+fi
+
+echo "Processing user: $loggedInUser"
+
+if dscl . -read /Users/$loggedInUser OriginalNodeName >/dev/null 2>&1; then
+
+echo "Mobile account detected."
+
+attributes=(
+
+OriginalNodeName
+
+SMBPrimaryGroupSID
+
+SMBGroupRID
+
+SMBUserRID
+
+SMBUserSID
+
+cached_groups
+
+cached_auth_policy
+
+CopyTimestamp
+
+AltSecurityIdentities
+
+)
+
+for attribute in "${attributes[@]}"; do
+
+dscl . -delete /Users/$loggedInUser "$attribute" 2>/dev/null
+
+done
+
+chown -R "$loggedInUser":staff "/Users/$loggedInUser"
+
+echo "Successfully converted $loggedInUser to local account."
+
+else
+
+echo "Account is already local."
+
+fi
+
+exit 0
